c69f37f32ebaeeebba980151a84155b00936b4e9
molekylaigor <91902529+shimkoeugen@users.noreply.github.com>
2022-01-10 19:55:52 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,90 @@
+//let a = prompt('Введите значение a')
+//let p = a*4
+
+//alert(p)
+
+//let a = prompt('Введите значение a')
+//let p = a*a
+
+//alert(p )
+
+//let a = prompt('Введите значение a')
+//let b = prompt('Введите значение b')
+//let s = a*b
+//let p = 2*(1*a+1*b)
+
+//alert(s + '  ' + p)
+
+//let a = prompt('Введите значение a')
+//let v = a*a*a
+//let s = 6*a*a
+
+//alert(v + '  ' + s)
+
+//let a = prompt('Введите значение a')
+//let b = prompt('Введите значение b')
+//let c = prompt('Введите значение c')
+//let v = a*b*c
+//let s = 2*(1*a*b+1*b*c+1*a*c)
+
+//alert(v + '  ' + s)
+
+//let R = prompt('Введите значение R')
+//let p = 3.14
+//let L  = 2*p*R 
+//let S = p*R*R
+
+//alert(L + '  ' + S)
+
+//let a = prompt('Введите значение a')
+//let b = prompt('Введите значение b')
+
+//let sr = (1*a+1*b)/2
+
+//alert(sr)
+
+//let a = prompt('Введите значение a')
+//let b = prompt('Введите значение b')
+//let k = Math.sqrt(a*b)
+
+//alert(k)
+
+
+//let b = prompt('Введите значение b')
+//let a = prompt('Введите значение a')
+//let s = 1*a*a+1*b*b
+//let r = 1*a*a-1*b*b
+//let p = a*a*b*b
+//let ch = a*a/b*b
+
+//alert(`Сума квадратів ${s} Різниця квадратів ${r} Добуток квадратів ${p} Частка квадратів ${ch}`)
+
+
+
+//let b = prompt('Введите значение b')
+//let a = prompt('Введите значение a')
+//let s = 1*a+1*b
+//let r = 1*a-1*b
+//let p = a*b
+//let ch = a/b
+
+//alert(`Сума модулей ${s} Різниця модулей ${r} Добуток модулей ${p} Частка модулей ${ch}`)
+
+
+//let a = prompt('Введите значение a')
+//let b = prompt('Введите значение b')
+//let c = Math.sqrt(a*a+b*b)
+//let P = 1*a+1*b+1*c
+
+//alert(`Гіпотенуза: ${c} Периметр: ${P}`)
+
+//let L = prompt('Введите значение L')
+//let p = 3.14
+//let R = L/2*p
+//let S = p*R*R
+
+//alert( R + '  ' + S)
+
+
+
+
